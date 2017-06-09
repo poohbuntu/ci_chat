56f49f1d94f1ec9338407f283394effa444300cb
furita <cs.mcru@gmail.com>
2017-06-09 14:46:39 +0700

    edit style

--- application/views/chat/chat_form.php
+++ application/views/chat/chat_form.php
@@ -3,9 +3,9 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Chat</title>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
+		<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css"/>
+		<script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
 		<style>
 			body{
 				background-color: #f2f2f2;
@@ -21,6 +21,7 @@
 				width: 300px;
 				*/
 				height: 400px;
+
 				overflow: auto;
 			}
 			#content p{
@@ -39,18 +40,13 @@
 				*/
 				overflow: auto;
 			}
-			#message button{
+			#message {
 				margin: 1em 0 0 0;
 			}
 			#message #chat_message{
 				/*
 				border: 1px solid;
 				*/
-				padding: 6px;
-				text-decoration: none;
-			}
-			button{
-				border: none;
 				padding: 6px;
 				text-decoration: none;
 			}
@@ -70,7 +66,7 @@
           $.ajax({
             type:'post',
             data:{chat_message:chat_message,old:old},
-            url:'<?php echo site_url('chat/find_answer'); ?>',
+            url:'<?php echo site_url('/chat/find_answer'); ?>',
             success:function(result) {
               $('#content').html(old
 								+'<p class="text-left" id="men-message">'+'you: '+chat_message+'</p>'
@@ -85,7 +81,7 @@
 		</script>
 	</head>
 	<body>
-		<div class="col-xs-12 col-sm-12 col-md-12">
+		<div class="col-xs-6 col-sm-6 col-md-6">
 			<div class="page-header">
 				<h1 class="text-center">สอบถามงานทะเบียน<small> ฝากคำถามสั้นๆ</small></h1>
 			</div>
@@ -96,7 +92,7 @@
 	      <form method="post">
 					<div class="form-group">
 						<div class="col-xs-10 col-sm-10 col-md-10">
-	        		<input type="text" class="form-control" id="chat_message" name="chat_message" rows="2" cols="25" placeholder="ส่งข้อความ"></textarea>
+	        		<input type="text" class="form-control" id="chat_message" name="chat_message" placeholder="ส่งข้อความ"></textarea>
 						</div>
 						<input type="submit" class="btn btn-primary" id="submit" value="Send">
 					</div>
