5fabc03964655496426a1cb964159e25cde6004d
Ogawa <cs.mcru@gmail.com>
2017-07-30 16:42:14 +0700

    gif
    gif

--- application/views/chat/chat_form.php
+++ application/views/chat/chat_form.php
@@ -65,17 +65,24 @@
           event.preventDefault();
 					var chat_message = $('#chat_message').val();
 					var old = $('#content').html();
+
+					$("#gif").attr('src',"<?php echo base_url();?>assets/img/Crying_baby_2.gif");
+
           $.ajax({
             type:'post',
+						//dataType:'json',
+						//contentType: 'application/json',
             data:{chat_message:chat_message,old:old},
             url:'<?php echo site_url('/chat/find_answer'); ?>',
             success:function(result) {
+
               $('#content').html(old
 								+'<p class="text-left" id="men-message">'+'you: '+chat_message+'</p>'
 								+'<p class="text-left" id="bot-message">'+result+'</p>'
 							);
 							$("#content").animate({ scrollTop: $("#content")[0].scrollHeight}, 1000);
     					$('#chat_message').val('');
+
             }
           });
 				});
@@ -83,23 +90,31 @@
 		</script>
 	</head>
 	<body>
-		<div class="col-xs-6 col-sm-6 col-md-6">
-		<div class="col-xs-12 col-sm-12 col-md-12">
-			<div class="page-header">
-				<h1 class="text-center">สอบถามงานทะเบียน<small> ฝากคำถามสั้นๆ</small></h1>
-			</div>
-			<div class="well" id="content">
-					<p class="text-left" id="bot-message">สวัสดี</p>
-			</div>
-			<div id="message">
-	      <form method="post">
-					<div class="form-group">
-						<div class="col-xs-10 col-sm-10 col-md-10">
-	        		<input type="text" class="form-control" id="chat_message" name="chat_message" placeholder="ส่งข้อความ">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12">
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<div class="page-header">
+							<h1 class="text-center">สอบถามงานทะเบียน<small> ฝากคำถามสั้นๆ</small></h1>
 						</div>
-						<input type="submit" class="btn btn-primary" id="submit" value="Send">
+						<div class="well" id="content">
+								<p class="text-left" id="bot-message">สวัสดี</p>
+						</div>
+						<div id="message">
+					    <form method="post">
+								<div class="form-group">
+									<div class="col-xs-8 col-sm-8 col-md-8">
+					        	<input type="text" class="form-control" id="chat_message" name="chat_message" placeholder="ส่งข้อความ">
+									</div>
+									<input type="submit" class="btn btn-primary" id="submit" value="Send">
+								</div>
+					    </form>
+						</div>
 					</div>
-	      </form>
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<img id="gif" src="<?php echo base_url();?>assets/img/Cave_man.gif" class="img-responsive">
+					</div>
+				</div>
 			</div>
 		</div>
 	</body>
